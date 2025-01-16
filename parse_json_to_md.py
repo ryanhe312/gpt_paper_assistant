0dbbcb260bb07a587367e2b7a7dce89e3849985a
ryanhe312 <ryanhe312@gmail.com>
2025-01-16 20:28:06 +0800

    use deepseek

--- parse_json_to_md.py
+++ parse_json_to_md.py
@@ -115,8 +115,6 @@ def render_md_string(papers_dict):
         "# Personalized Daily Arxiv Papers "
         + datetime.today().strftime("%m/%d/%Y")
         + "\n\nThis project is adapted from [tatsu-lab/gpt_paper_assistant](https://github.com/tatsu-lab/gpt_paper_assistant).\n\n"
-        + "About me on [Bilibili](https://space.bilibili.com/823532). Help keep the website running:\n\n"
-        + f"""<a href="https://www.buymeacoffee.com/Variante"><img src="https://img.buymeacoffee.com/button-api/?text=Help cover GPT-4 cost&emoji={random_emoji}&slug=Variante&button_colour={generate_background_for_white_foreground()}&font_colour=ffffff&font_family={random_font}&outline_colour=000000&coffee_colour=FFDD00" /></a>\n"""
         + "\n\n## Topics\n\nPaper selection prompt and criteria (jump to the section by clicking the link):\n\n"
         + criteria_string
         + "\n---\n"
